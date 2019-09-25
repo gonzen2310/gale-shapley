16a71da6bc8db71f804a26087e500f9685d5ef38
gonzen2310 <gonzaloreyesllauca@gmail.com>
2019-09-24 22:29:55 -0400

    Added gnuplot graph

--- Labs/Lab0/run-gs1.sh
+++ Labs/Lab0/run-gs1.sh
@@ -3,10 +3,11 @@
 echo Gale Shapley Algorithm
 echo Writing times to data.txt
 
-> data.txt
 for i in 1000 1500 2000 2500 5000
 do
     node gs1.js $i >> data.txt
 done
+
+gnuplot -persist model.gpt
 
 echo Done!
